16df72f97ef3d3e26368aee99f7af009c775bb0e
Andrea Michael Maria Molino <andreamolino123@gmail.com>
2024-01-15 17:50:49 +0100

    Implemented LibFile - get_filesize() is_file_open() get_filesize_from_file()

---
+++ LibC/LibFile/lib_file.c
@@ -0,0 +1,57 @@
+/* Libs - LibC/LibFile
+ * Copyright (C) 2024  Andrea Michael Maria Molino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "lib_file.h"
+
+long get_filesize(FILE* file){
+    is_file_open(file);
+
+    if(fseek(file, 0, SEEK_END) < 0) return -1;
+
+    long filesize = ftell(file); 
+    if(filesize < 0) return -1;
+
+
+    if(fseek(file, 0, SEEK_SET) < 0) return -1;
+
+    return filesize; 
+}
+
+long get_filesize_from_file(const char* file){
+    FILE* file_to_open = fopen(file, "r");
+    is_file_open(file_to_open);
+
+    if(fseek(file_to_open, 0, SEEK_END) < 0) return -1;
+
+    long filesize = ftell(file_to_open); 
+    if(filesize < 0) return -1;
+
+
+    if(fseek(file_to_open, 0, SEEK_SET) < 0) return -1;
+
+    fclose(file_to_open);
+    return filesize;
+}
+
+void is_file_open(FILE* file_to_open){
+    if(file_to_open == NULL){
+        fprintf(stderr, "[ERROR] Could not open: %s", strerror(errno));
+        return;
+    }
+
+}
+
