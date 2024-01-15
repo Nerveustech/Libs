b81cd24b4b08c180afd7d75e9225790d0e308c13
Andrea Michael Maria Molino <andreamolino123@gmail.com>
2024-01-15 18:01:27 +0100

    Implemented LibMath - compute_compression_ratio()

---
+++ LibC/LibMath/lib_math.c
@@ -0,0 +1,22 @@
+/* Libs - LibC/LibFile
+ * Copyright (C) 2024  Andrea Michael Maria Molino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "lib_math.h"
+
+float compute_compression_ratio(float uncompressed_size, float compressed_size){
+    return uncompressed_size/compressed_size;
+}
