d9b44b97802ac4a394b0d56b4327f0598b4bb402
Nerveustech <andreamolino123@gmail.com>
2025-06-22 19:05:09 +0200

    Fix and Improve log.h

--- LibC/logging/log.h
+++ LibC/logging/log.h
@@ -25,6 +25,7 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 ///
@@ -32,17 +33,13 @@
 ///
 
 #if defined(WIN64) || defined(_WIN64) || defined(__MINGW64__)
-	#define WINDOWS_64
+    #define WINDOWS_64
 #elif defined(WIN32) || defined(_WIN32) || defined(__MINGW32__)
-	#define WINDOWS_32
+    #define WINDOWS_32
 #endif
 
 #if defined(WINDOWS_64) || defined(WINDOWS_32)
     #define WINDOWS
-#endif
-
-#ifdef __linux__
-    #define LINUX
 #endif
 
 typedef enum {
@@ -57,16 +54,21 @@ typedef enum {
 ///End Macro section
 ///
 
-#if defined(LINUX)
+#if defined(__linux__)
 
-    #define C_RESET  "\x1B[0m"
-    #define C_RED    "\x1B[1;31m"
-    #define C_GREEN  "\x1B[1;32m"
-    #define C_YELLOW "\x1B[1;33m"
-    #define C_BLUE   "\x1B[1;34m"
-    #define C_ORANGE "\x1B[1;93m"
+    #define LOG_COLOR_RESET  "\x1B[0m"
+    #define LOG_COLOR_RED    "\x1B[1;31m"
+    #define LOG_COLOR_GREEN  "\x1B[1;32m"
+    #define LOG_COLOR_YELLOW "\x1B[1;33m"
+    #define LOG_COLOR_BLUE   "\x1B[1;34m"
+    #define LOG_COLOR_ORANGE "\x1B[1;93m"
 
-    void print(LogType type, const char* format, ...);
+    void info(const char* format, ...);
+    void debug(const char* format, ...);
+    void warning(const char* format, ...);
+    void error(const char* format, ...);
+    void critical(const char* format, ...);
+
     void log_file(LogType type, const char* file,  const char* format, ...);
 
 #elif defined(WINDOWS)
@@ -113,58 +115,99 @@ typedef enum {
     #include <Windows.h>
 
 
-    #define C_RESET  7
-    #define C_RED    4
-    #define C_GREEN  2
-    #define C_YELLOW 6
-    #define C_BLUE   1
-    #define C_ACQUA FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY
-    
-    void print(LogType type, CONST WCHAR* format, ...);
+    #define LOG_COLOR_RESET  7
+    #define LOG_COLOR_RED    4
+    #define LOG_COLOR_GREEN  2
+    #define LOG_COLOR_YELLOW 6
+    #define LOG_COLOR_BLUE   1
+    #define LOG_COLOR_ACQUA FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY
+
+    void info(CONST WCHAR* format, ...);
+    void debug(CONST WCHAR* format, ...);
+    void warning(CONST WCHAR* format, ...);
+    void error(CONST WCHAR* format, ...);
+    void critical(CONST WCHAR* format, ...);
+
     void log_file(LogType type, CONST WCHAR* file, CONST WCHAR* format, ...);
 #endif
 
 #ifdef LIB_PRINT_IMPLEMENTATION
 
-#ifdef LINUX
-void print(LogType type, const char* format, ...)
+#ifdef __linux__
+void info(const char* format, ...)
 {
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
 
     va_list args;
     va_start(args, format);
 
-    switch (type)
-    {
-        case Success:
-            fprintf(stdout, "%s[SUCCESS]%s ", C_GREEN, C_RESET);
-            vfprintf(stdout, format, args);
-            break;
-    
-        case Error:
-            fprintf(stderr, "%s[ERROR]%s ", C_RED, C_RESET);
-            vfprintf(stderr, format, args);
-            break;
-        
-        case Info:
-            fprintf(stdout, "%s[INFO]%s ", C_YELLOW, C_RESET);
-            vfprintf(stdout, format, args);
-            break;
-        
-        case Warning:
-            fprintf(stdout, "%s[WARNING]%s ", C_ORANGE, C_RESET);
-            vfprintf(stdout, format, args);
-            break;
-        
-        case Custom:
-            vfprintf(stdout, format, args);
-            break;
-        
-        default:
-            __builtin_unreachable();
+    fprintf(stdout, "%s[INFO]%s ", LOG_COLOR_BLUE, LOG_COLOR_RESET);
+    vfprintf(stdout, format, args);
+    va_end(args);
+}
+
+void debug(const char* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
     }
 
+    va_list args;
+    va_start(args, format);
+
+    fprintf(stdout, "%s[DEBUG]%s ", LOG_COLOR_BLUE, LOG_COLOR_RESET);
+    vfprintf(stdout, format, args);
     va_end(args);
-    return;
+}
+
+void warning(const char* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    fprintf(stdout, "%s[WARNING]%s ", LOG_COLOR_YELLOW, LOG_COLOR_RESET);
+    vfprintf(stdout, format, args);
+    va_end(args);
+}
+
+void error(const char* format, ...)
+{
+    if(format == NULL){
+        fprintf(stdout, "%s[ERROR]%s %s format is NULL!", LOG_COLOR_RED, LOG_COLOR_RESET, __func__);
+        return;
+    }
+    
+    va_list args;
+    va_start(args, format);
+
+    fprintf(stdout, "%s[ERROR]%s ", LOG_COLOR_RED, LOG_COLOR_RESET);
+    vfprintf(stdout, format, args);
+    va_end(args);
+}
+
+void critical(const char* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
+    
+    va_list args;
+    va_start(args, format);
+
+    fprintf(stdout, "%s[CRITICAL]%s ", LOG_COLOR_RED, LOG_COLOR_RESET);
+    vfprintf(stdout, format, args);
+    va_end(args);
+    abort();
 }
 
 void log_file(LogType type, const char* file,  const char* format, ...)
@@ -172,7 +215,7 @@ void log_file(LogType type, const char* file,  const char* format, ...)
     FILE* file_to_open = fopen(file, "a");
 
     if(file_to_open == NULL){
-        print(Error, "Could not open: %s for log\n", file);
+        error("Could not open: %s for log\n", file);
         return;
     }
 
@@ -217,57 +260,107 @@ void log_file(LogType type, const char* file,  const char* format, ...)
 #endif
 
 #ifdef WINDOWS
-void print(LogType type, CONST WCHAR* format, ...)
+void info(CONST WCHAR* format, ...)
 {
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
+
     va_list args;
     va_start(args, format);
-
     HANDLE hConsole;
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    switch (type)
-    {
-        case Success:
-            SetConsoleTextAttribute(hConsole, C_GREEN);
-            fwprintf(stdout, L"[SUCCESS] ");
-            SetConsoleTextAttribute(hConsole, C_RESET);
-            vfwprintf(stdout, format, args);
-            break;
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_ACQUA);
+    fwprintf(stdout, L"[INFO] ");
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RESET);
+    vfwprintf(stdout, format, args);
     
-        case Error:
-            SetConsoleTextAttribute(hConsole, C_RED);
-            fwprintf(stderr, L"[ERROR] ");
-            SetConsoleTextAttribute(hConsole, C_RESET);
-            vfwprintf(stderr, format, args);
-            break;
-        
-        case Info:
-            SetConsoleTextAttribute(hConsole, C_ACQUA);
-            fwprintf(stdout, L"[INFO] ");
-            SetConsoleTextAttribute(hConsole, C_RESET);
-            vfwprintf(stdout, format, args);
-            break;
-        
-        case Warning:
-            SetConsoleTextAttribute(hConsole, C_YELLOW);
-            fwprintf(stdout, L"[WARNING] ");
-            SetConsoleTextAttribute(hConsole, C_RESET);
-            vfwprintf(stdout, format, args);
-            break;
-        
-        case Custom:
-            vfwprintf(stdout, format, args);
-            break;
-        
-        default:
-            __assume(0);
+    va_end(args);
+}
+
+void debug(CONST WCHAR* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
     }
 
+    va_list args;
+    va_start(args, format);
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_ACQUA);
+    fwprintf(stdout, L"[DEBUG] ");
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RESET);
+    vfwprintf(stdout, format, args);
+    
     va_end(args);
-
-    return;
-
 }
+
+void warning(CONST WCHAR* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
+
+    va_list args;
+    va_start(args, format);
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_YELLOW);
+    fwprintf(stdout, L"[WARNING] ");
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RESET);
+    vfwprintf(stdout, format, args);
+    
+    va_end(args);
+}
+
+void error(CONST WCHAR* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
+
+    va_list args;
+    va_start(args, format);
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RED);
+    fwprintf(stdout, L"[ERROR] ");
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RESET);
+    vfwprintf(stdout, format, args);
+    
+    va_end(args);
+}
+
+void critical(CONST WCHAR* format, ...)
+{
+    if(format == NULL){
+        error("%s: format is NULL!", __func__);
+        return;
+    }
+
+    va_list args;
+    va_start(args, format);
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RED);
+    fwprintf(stdout, L"[CRITICAL] ");
+    SetConsoleTextAttribute(hConsole, LOG_COLOR_RESET);
+    vfwprintf(stdout, format, args);
+    
+    va_end(args);
+    abort();
+}
+
 
 void log_file(LogType type, CONST WCHAR* file, CONST WCHAR* format, ...)
 {
@@ -275,7 +368,7 @@ void log_file(LogType type, CONST WCHAR* file, CONST WCHAR* format, ...)
     _wfopen_s(&file_to_open, file, L"a");
 
     if(file_to_open == NULL){
-        print(Error, L"Could not open: %s for log\n", file);
+        error(L"Could not open: %s for log\n", file);
         return;
     }
 
